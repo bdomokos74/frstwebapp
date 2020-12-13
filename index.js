969b22803735c6a6ffd7b4f64554d43470e8bf41
Balint Domokos <bdomokos74@gmail.com>
2020-12-13 15:27:47 +0100

    Add new text

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 http.createServer(function(request, response) {
     response.writeHead(200, { "Content-Type": "text/html" });
-    response.end("<html><body><h1>frstWebApp</h1></body></html>");
+    response.end("<html><body><h1>frstWebApp new version</h1></body></html>");
 }).listen(port);
 
 console.log(`Server running at http://localhost:${port}`);
